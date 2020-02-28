e137731480b05c316139bf8cb30bf89036a4c0e9
thejcode24 <thejcode24@gmail.com>
2020-02-27 20:41:56 -0800

    Functionality completed

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,28 +1,85 @@
 
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var goalColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
-var resetBtn = document.querySelector("#resetButton");
+var resetBtn = document.querySelector("#resetBtn");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
 // Event listener for reset button
-resetBtn.addEventListener("click", function() {
-    // Generate array of random colors
-    colors = generateRandomColors(6);
+resetBtn.addEventListener("click", function () {
     
+    // Generate array of random colors
+    colors = generateRandomColors(numSquares);
+
     // Set goal color to random color
     goalColor = pickColor();
 
     colorDisplay.textContent = goalColor;
     messageDisplay.textContent = "";
 
-    for(var i=0; i < squares.length; i++) {
+    for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
     h1.style.backgroundColor = "#232323";
 
+});
+
+easyBtn.addEventListener("click", function () {
+
+    // Set number of squares
+    numSquares = 3;
+
+    // Remove chosen option style from "hard" button
+    hardBtn.classList.remove("selected");
+
+    // Show easy button as chosen option
+    easyBtn.classList.add("selected");
+
+    // Erase message
+    messageDisplay.textContent = "";
+
+    // Generate 3 RGB values to fill 3 squares for "easy" mode
+    colors = generateRandomColors(numSquares);
+    goalColor = pickColor();
+
+    // Display RGB value
+    colorDisplay.textContent = goalColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        // If colors exists, set square color as color
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function () {
+
+    numSquares = 6;
+
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    messageDisplay.textContent = "";
+
+    // Generate 3 RGB values to fill 3 squares for "easy" mode
+    colors = generateRandomColors(numSquares);
+    goalColor = pickColor();
+
+    // Display RGB value
+    colorDisplay.textContent = goalColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        // If colors exists, set square color as color
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
 });
 
 colorDisplay.textContent = goalColor;
@@ -47,12 +104,12 @@ for (var i = 0; i < squares.length; i++) {
             resetBtn.textContent = "Play Again?";
 
 
-        // Incorrect guess "erases" square by changing color
-        // to background color
+            // Incorrect guess "erases" square by changing color
+            // to background color
         } else {
             this.style.background = "#232323";
             messageDisplay.textContent = "Try Again";
-            
+
         }
     });
 }
