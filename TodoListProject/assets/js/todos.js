3eeb293c4a92bfcbc378f8202385e51f5efb025f
thejcode24 <thejcode24@gmail.com>
2020-03-03 10:45:07 -0800

    Basic jQuery added

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,13 +1,17 @@
+// Load jQuery when Dom is safe to manipulate
+$(function() {
 
-$(document).ready(function() {
+    // Event delegation used to prevent event propagation or "bubbling"
+    // Set event listener on the whole list ul, then pass "li"as second
+    // argument and if clicked element is a "li", run function
 
     // Check off specific todo by clicking
-    $("li").click(function() {
+    $("ul").on("click", "li", function() {
         $(this).toggleClass("completed");
     });
 
     // Click on X to delete todo
-    $("span").click(function(event) {
+    $("ul").on("click", "span", function(event) {
         
         // Get parent jquery element which is li in this case
         $(this).parent().fadeOut(500, function() {
@@ -20,8 +24,23 @@ $(document).ready(function() {
         // Prevents event bubbling which means click is going
         // through all layers of DOM down to html
         event.stopPropagation();
-
     })
 
+    // Get input from text input box
+    $("input[type='text']").keypress(function(event) {
+
+        // Check if Enter key is pressed
+        if(event.which === 13) {
+
+            // Get text input 
+            var textInput = $(this).val();
+
+            // Clear input
+            $(this).val("");
+
+            // Create a new li and add to ul
+            $("ul").append("<li><span>x</span> "+ textInput + "</li>");
+        }  
+    })
 });
 
