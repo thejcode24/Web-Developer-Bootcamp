8b6c2b4e853d1d4dafb0ed9b60573594526aee7f
James Jung <thejcode24@gmail.com>
2020-03-30 18:22:21 -0700

    Request examples added

---
+++ RequestAPI/FirstRequest/request_promise.js
@@ -0,0 +1,10 @@
+
+var rp = require('request-promise');
+
+rp('http://www.google.com')
+    .then(function (htmlString) {
+        // Process html...
+    })
+    .catch(function (err) {
+        // Crawling failed...
+    });
